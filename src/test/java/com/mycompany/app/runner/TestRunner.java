02521a96f1786d1142653234bfa139bfa11d248e
davbaster <davbaster@gmail.com>
2024-03-21 16:15:26 -0600

    Update TestRunner.java

--- src/test/java/com/mycompany/app/runner/TestRunner.java
+++ src/test/java/com/mycompany/app/runner/TestRunner.java
@@ -2,11 +2,6 @@ package com.mycompany.app.runner;
 
 import io.cucumber.junit.platform.engine.Cucumber;
 
-@Cucumber(
-    glue = "com.mycompany.app.stepdefinitions",
-    features = "src/test/resources/features/",
-    extraGlue = "com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results/ManualRunnerTest_OctaneGherkinResults.xml"
-)
 public class TestRunner {
     // Configuration is included directly in the annotation parameters
 }
