3e6f1fd1b5d080827868a08498d13d0eeca2acb6
davbaster <davbaster@gmail.com>
2024-03-21 16:17:44 -0600

    Update TestRunner.java

--- src/test/java/com/mycompany/app/runner/TestRunner.java
+++ src/test/java/com/mycompany/app/runner/TestRunner.java
@@ -2,6 +2,7 @@ package com.mycompany.app.runner;
 
 import io.cucumber.junit.platform.engine.Cucumber;
 
+@Cucumber
 public class TestRunner {
     // Configuration is included directly in the annotation parameters
 }
