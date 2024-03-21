1b86c0ed274e3868add28756e66c05df4c86c213
davbaster <davbaster@gmail.com>
2024-03-21 15:56:30 -0600

    Update TestRunner.java

--- src/test/java/com/mycompany/app/runner/TestRunner.java
+++ src/test/java/com/mycompany/app/runner/TestRunner.java
@@ -4,5 +4,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        glue = "com.mycompany.app.stepdefinitions",
+        plugin = "com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results/ManualRunnerTest_OctaneGherkinResults.xml"
+)
 public class TestRunner {
 }
+
