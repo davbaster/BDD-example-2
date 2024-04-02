e6e9f86ebb3af60c3b47c67bf8b47db10bee51c1
davbaster <davbaster@gmail.com>
2024-04-02 12:19:26 -0600

    Create TestRunner.java

---
+++ src/test/java/com/mycompany/app/runner/TestRunner.java
@@ -0,0 +1,9 @@
+package com.mycompany.app.runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class TestRunner{
+  //No additional code needed here when using Junit 5 with Cucumber
+}
