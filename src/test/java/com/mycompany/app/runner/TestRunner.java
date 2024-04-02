d68eb9344d020176d5b6ed39f3692cbf3c55a4fb
davbaster <davbaster@gmail.com>
2024-04-02 17:41:01 -0600

    Update TestRunner.java
    added son imports

--- src/test/java/com/mycompany/app/runner/TestRunner.java
+++ src/test/java/com/mycompany/app/runner/TestRunner.java
@@ -1,8 +1,21 @@
 package com.mycompany.app.runner;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+//import io.cucumber.junit.platform.engine.Cucumber;
 
-@Cucumber
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("com.mycompany.app.runner")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.mycompany.app.runner")
 public class TestRunner {
   // Nothing is required here when using JUnit 5 with Cucumber
 }
